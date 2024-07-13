7ee12f68436cbdeec339047884d98a13d533b580
Nguyen-Dang-Trieu <121329547+Nguyen-Dang-Trieu@users.noreply.github.com>
2024-07-13 10:31:50 +0700

    Update Bai3.c

--- Bai3.c
+++ Bai3.c
@@ -2,59 +2,53 @@
 
 struct MyMap
 {
-   unsigned int  b31:1;
-   unsigned int  b30:1;
-   unsigned int  b29:1;
-   unsigned int  b28:1;
-   unsigned int  b27:1;
-   unsigned int  b26:1;
-   unsigned int  b25:1;
-   unsigned int  b24:1;
-   unsigned int  b23:1;
-   unsigned int  b22:1;
-   unsigned int  b21:1;
-   unsigned int  b20:1;
-   unsigned int  b19:1;
-   unsigned int  b18:1;
-   unsigned int  b17:1;
-   unsigned int  b16:1;
-   unsigned int  b15:1;
-   unsigned int  b14:1;
-   unsigned int  b13:1;
-   unsigned int  b12:1;
-   unsigned int  b11:1;
-   unsigned int  b10:1;
-   unsigned int  b9:1;
-   unsigned int  b8:1;
-   unsigned int  b7:1;
-   unsigned int  b6:1;
-   unsigned int  b5:1;
-   unsigned int  b4:1;
-   unsigned int  b3:1;
-   unsigned int  b2:1;
-   unsigned int  b1:1;
-   unsigned int  b0:1;
+   unsigned int  b31: 1;
+   unsigned int  b30: 1;
+   unsigned int  b29: 1;
+   unsigned int  b28: 1;
+   unsigned int  b27: 1;
+   unsigned int  b26: 1;
+   unsigned int  b25: 1;
+   unsigned int  b24: 1;
+   unsigned int  b23: 1;
+   unsigned int  b22: 1;
+   unsigned int  b21: 1;
+   unsigned int  b20: 1;
+   unsigned int  b19: 1;
+   unsigned int  b18: 1;
+   unsigned int  b17: 1;
+   unsigned int  b16: 1;
+   unsigned int  b15: 1;
+   unsigned int  b14: 1;
+   unsigned int  b13: 1;
+   unsigned int  b12: 1;
+   unsigned int  b11: 1;
+   unsigned int  b10: 1;
+   unsigned int  b9: 1;
+   unsigned int  b8: 1;
+   unsigned int  b7: 1;
+   unsigned int  b6: 1;
+   unsigned int  b5: 1;
+   unsigned int  b4: 1;
+   unsigned int  b3: 1;
+   unsigned int  b2: 1;
+   unsigned int  b1: 1;
+   unsigned int  b0: 1;
 
 
 };
 
 
-void printBits( int a )
-{
-   int i;
- 
-   for ( i = 31; i >= 0; i-- )
-   {
+void printBits( int a ) {
+   for (int i = 31; i >= 0; i-- ) {
 	 if ( (a & (1<<i) ) != 0 )
 	    putchar('1');
 	 else
 	    putchar('0');
    }
- 
 }
  
-int main( int argc, char* argv[] )
-{
+int main( int argc, char* argv[]){
    struct MyMap r;
 
    printf( "sizeof(r) = %lu\n", sizeof(r) );
